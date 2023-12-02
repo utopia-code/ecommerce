cee62de8c6f6581a98630740f98881751665e831
utopia-code <utopiacode.developer@gmail.com>
2023-12-02 21:47:43 +0100

    PEC4 - Exer4: generate article-item in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleItemComponent } from './article/article-item/article-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleItemComponent
   ],
   imports: [
     BrowserModule,
