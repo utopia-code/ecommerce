a3faebe2472faafe55cab03e8295368c41f61553
utopia-code <utopiacode.developer@gmail.com>
2023-12-05 18:30:05 +0100

    PEC4 - Exer7: added new component article-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticleItemComponent } from './article/article-item/article-item.component';
+import { ArticleListComponent } from './article/article-list/article-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleItemComponent
+    ArticleItemComponent,
+    ArticleListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/article/article-list/article-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-list',
+  templateUrl: './article-list.component.html',
+  styleUrl: './article-list.component.scss'
+})
+export class ArticleListComponent implements OnInit{
+
+  ngOnInit(): void {
+    
+  }
+}
