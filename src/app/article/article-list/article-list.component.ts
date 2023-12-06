c9e9c64919d4d6e115c368375050ea5137303aa5
utopia-code <utopiacode.developer@gmail.com>
2023-12-06 18:31:26 +0100

    PEC4 - Exer7-3: modified component article-list: - import ArticleQuantityChange - create methode onArticleQuantity() that print article and amount articles - add event binding in template

--- src/app/article/article-list/article-list.component.ts
+++ src/app/article/article-list/article-list.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from '../../models/article';
+import { ArticleQuantityChange } from '../../models/article-quantity-change';
 
 @Component({
   selector: 'app-article-list',
   template: `
     <div class="section-articles">
-      <app-article-item *ngFor="let article of articles" [article]="article"></app-article-item>
+      <app-article-item *ngFor="let article of articles" 
+        [article]="article"
+        (articleQuantity)="onArticleQuantity($event)"></app-article-item>
     </div>`,
   styles: [`
     .section-articles {
@@ -26,10 +29,14 @@ export class ArticleListComponent implements OnInit{
   ngOnInit() {
     this.articles = [
       new Article('Abstract de Beatriz Jara', './assets/pexels-beatriz-jara-8882691.jpg', 185, true, 1),
-      new Article('Sounds de Eberhard Grossgasteiger', './assets/pexels-eberhard-grossgasteiger-2086361.jpg', 375, false, 1),
+      new Article('Still life de Eberhard Grossgasteiger', './assets/pexels-eberhard-grossgasteiger-2086361.jpg', 375, false, 1),
       new Article('Portrait de Matt Fernandes', './assets/pexels-matt-fernandes-2807495.jpg', 295, true, 1)
-  ];
-
-    console.log(this.articles)
+    ];
   }
+
+  onArticleQuantity(articleChange: ArticleQuantityChange) {
+    console.log('Article: ' + JSON.stringify(articleChange.articleObj))
+    console.log('Article quantity: ' + articleChange.totalQuantity)
+  }
+  
 }
