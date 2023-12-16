70b33d7841cb48c2d04f9fc36f584bbfb8e51640
utopia-code <utopiacode.developer@gmail.com>
2023-12-16 19:11:16 +0100

    PEC5 - Exer3:  create new template form

--- src/app/article/article-new-template/article-new-template.component.ts
+++ src/app/article/article-new-template/article-new-template.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from '../../models/article';
 
 @Component({
   selector: 'app-article-new-template',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './article-new-template.component.scss'
 })
 export class ArticleNewTemplateComponent {
+  public article: Article;
 
+  createArticle(templateForm) {
+    console.log('Article form', templateForm.value );
+    if (templateForm.valid) {
+      console.log('Creating Article', this.article);
+    } else {
+      console.error('Template form is in an invalid state')
+    }
+  }
 }
