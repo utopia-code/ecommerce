ec79ed21feafa11e9bdd2b6368f330d51f8fd8ac
utopia-code <utopiacode.developer@gmail.com>
2023-12-17 14:30:01 +0100

    PEC5 - Exer3: Added validation inputs

--- src/app/article/article-new-template/article-new-template.component.ts
+++ src/app/article/article-new-template/article-new-template.component.ts
@@ -12,9 +12,14 @@ export class ArticleNewTemplateComponent {
   createArticle(templateForm) {
     console.log('Article form', templateForm.value );
     if (templateForm.valid) {
-      console.log('Creating Article', this.article);
+      console.log('Creating Article', templateForm);
     } else {
       console.error('Template form is in an invalid state')
     }
+  }
+
+  validateImageUrl(url: string): boolean {
+    const imageUrlPattern = /^(https?|ftp):\/\/[A-Za-z0-9.-]+\.[A-Za-z]{2,3}(\/[^\/\s]*)*$/;
+    return imageUrlPattern.test(url);
   }
 }
