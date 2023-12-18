b8774b054239fb4d815997519d4601958caa7acb
utopia-code <utopiacode.developer@gmail.com>
2023-12-18 12:08:33 +0100

    PEC5 - Exer5: Create a new reactive form with FormGroup without Validators

--- src/app/article/article-new-reactive-template/article-new-reactive-template.component.ts
+++ src/app/article/article-new-reactive-template/article-new-reactive-template.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-article-new-reactive-template',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class ArticleNewReactiveTemplateComponent {
 
+  public reactiveForm: FormGroup = new FormGroup({
+    name: new FormControl(null, Validators.required),
+    price: new FormControl(0, Validators.required),
+    imageUrl: new FormControl(null, Validators.required),
+    isOnSale: new FormControl(null)
+  });
+
+  constructor() {}
+
+  onSubmit() {
+    console.log('Form Control Value', this.reactiveForm.value)
+  }
 }
