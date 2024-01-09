9091b566a956567628ddde8258a43b8f576c533e
utopia-code <utopiacode.developer@gmail.com>
2024-01-09 13:50:28 +0100

    PEC6 - Exer2: inject article-service in article-new-reactive.component and modify onSubmit method

--- src/app/article/article-new-reactive/article-new-reactive.component.ts
+++ src/app/article/article-new-reactive/article-new-reactive.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { Article } from '../../models/article';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { Validators, FormBuilder } from '@angular/forms';
 import { CustomValidator } from '../../custom-validator';
+import { ArticleService } from '../../services/article.service';
 
 @Component({
   selector: 'app-article-new-reactive',
@@ -14,24 +15,34 @@ export class ArticleNewReactiveComponent {
   public article: Article;
   public reactiveForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    this.createForm();
-    this.article = new Article(5, '', '', 0, false, 0);
-  }
+  constructor(
+    private fb: FormBuilder,
+    private articleService: ArticleService) {
+      this.createForm();
+    }
 
   createForm() {
     this.reactiveForm = this.fb.group({
+      id: 1,
       name: [null, [Validators.required, Validators.compose([CustomValidator.NameArticleValidator(['Prueba', 'Test', 'Mock', 'Fake'])])]],
       price: [null, [Validators.required, Validators.min(0.1)]],
       imageUrl: [null, [Validators.required, Validators.pattern(/^(https?|ftp):\/\/[A-Za-z0-9.-]+\.[A-Za-z]{2,3}(\/[^\/\s]*)*$/)]],
-      isOnSale: [null]
+      isOnSale: [null],
+      quantityInCart: 0
     });
   }
 
   onSubmit() {
-    console.log('Form Control Value', this.reactiveForm.value)
-    this.article = Object.assign({}, this.reactiveForm.value)
-    console.log('Article object', this.article)
+    if (this.reactiveForm.valid) {
+      const newArticle: Article = Object.assign({}, this.reactiveForm.value);
+      this.articleService.create(newArticle);
+
+      console.log('Form Control Value', this.reactiveForm.value)
+      console.log('Article object', this.article)
+      console.log(newArticle);
+    } else {
+      console.error('New Reactive Article form is in an invalid state');
+    }
   }
 
   get name() { return this.reactiveForm.get('name')}
