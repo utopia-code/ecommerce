0729f61ee2c88d60bca865eba51e01e89c7de943
utopia-code <utopiacode.developer@gmail.com>
2024-01-10 11:42:24 +0100

    PEC6 - Exer2: subcribe create methode of service in onSubmit(). Assing values of form to article

--- src/app/article/article-new-reactive/article-new-reactive.component.ts
+++ src/app/article/article-new-reactive/article-new-reactive.component.ts
@@ -23,23 +23,29 @@ export class ArticleNewReactiveComponent {
 
   createForm() {
     this.reactiveForm = this.fb.group({
-      id: 1,
       name: [null, [Validators.required, Validators.compose([CustomValidator.NameArticleValidator(['Prueba', 'Test', 'Mock', 'Fake'])])]],
       price: [null, [Validators.required, Validators.min(0.1)]],
       imageUrl: [null, [Validators.required, Validators.pattern(/^(https?|ftp):\/\/[A-Za-z0-9.-]+\.[A-Za-z]{2,3}(\/[^\/\s]*)*$/)]],
-      isOnSale: [null],
-      quantityInCart: 0
+      isOnSale: [null]
     });
   }
 
   onSubmit() {
     if (this.reactiveForm.valid) {
       const newArticle: Article = Object.assign({}, this.reactiveForm.value);
-      this.articleService.create(newArticle);
+      this.article = newArticle;
+      this.articleService.create(newArticle)
+      .subscribe(success => {
+          if (success) {
+            console.log('Form Control Value', this.reactiveForm.value);
+            console.log('Article object', this.article);
+            console.log(newArticle);
 
-      console.log('Form Control Value', this.reactiveForm.value)
-      console.log('Article object', this.article)
-      console.log(newArticle);
+            this.reactiveForm.reset();
+          } else {
+            console.error('New Reactive Article form is in an invalid state');
+          }
+        });
     } else {
       console.error('New Reactive Article form is in an invalid state');
     }
