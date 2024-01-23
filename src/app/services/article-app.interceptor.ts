3bcb9d17ef29d7b7ada83cc643ac29a76cf6a2f2
utopia-code <utopiacode.developer@gmail.com>
2024-01-23 20:58:49 +0100

    PEC7 - Exercise 2.4: add article-app interceptor

---
+++ src/app/services/article-app.interceptor.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { UserStoreService } from './user-store.service';
+
+@Injectable()
+export class ArticleAppInterceptor implements HttpInterceptor {
+
+  constructor(private userStore: UserStoreService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (this.userStore.token) {
+      console.log('INTERCEPTING, HAS TOKEN', this.userStore.token);
+      const authReq = req.clone({
+        headers: req.headers.set(
+          'X-AUTH-HEADER',
+          this.userStore.token
+        )
+      });
+      console.log('Making an authorized request');
+      req = authReq;
+    }
+    return next.handle(req);
+  }
+
+}
