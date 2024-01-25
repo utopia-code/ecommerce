d8b7462c217f2091cb8c931d47c75e881f428316
utopia-code <utopiacode.developer@gmail.com>
2024-01-25 12:24:04 +0100

    PEC7 - Exercise 2.b: add ActivatedRoute to article-detail and suscribe to getArticle to call article by id

--- src/app/articles/article-detail/article-detail.component.ts
+++ src/app/articles/article-detail/article-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from '../../models/article';
+import { ArticleService } from '../../services/article.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-article-detail',
@@ -10,9 +12,12 @@ export class ArticleDetailComponent implements OnInit {
 
   public article: Article;
 
-  constructor() {}
+  constructor(private articleService: ArticleService,
+              private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    
+    const articleID = this.route.snapshot.paramMap.get('id');
+    this.articleService.getArticle(+articleID)
+      .subscribe(article => this.article = article);
   }
 }
