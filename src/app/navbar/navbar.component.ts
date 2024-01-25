de65a1cbea61e2e59c499ac94728adb868ebd8d7
utopia-code <utopiacode.developer@gmail.com>
2024-01-25 11:26:44 +0100

    PEC7 - Exercise 2.b: modify navbar component to set up routes angular

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,24 +6,13 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-  @Output() changeContent = new EventEmitter<{content: string}>();
-  currentComponent: string = 'article-list';
+
   showMenuMobile = false;
 
-  itemsNav: Array<{link: string, component: string}> = [
-    {link: 'article-list', component: 'Artículos'},
-    {link: 'new-reactive', component: 'Nuevo artículo'},
-  ]
-
-  showComponent(componentName: string) {
-    this.currentComponent = componentName;
-    this.changeContent.emit({content: this.currentComponent});
-  }
-
-  isNavItemActive(componentName: string): boolean {
-    return this.currentComponent === componentName;
-  }
-
+  // itemsNav: Array<{link: string, component: string}> = [
+  //   {link: 'article-list', component: 'Artículos'},
+  //   {link: 'new-reactive', component: 'Nuevo artículo'},
+  // ]
   toogleMenu() {
     this.showMenuMobile = !this.showMenuMobile;
   }
