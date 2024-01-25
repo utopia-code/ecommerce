8d67c781e1afec22baa4e8f7c61da3fcb2b08228
utopia-code <utopiacode.developer@gmail.com>
2024-01-25 11:23:25 +0100

    PEC7 - Exercise 2.b: set up routes in app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ArticleListComponent } from './articles/article-list/article-list.component';
+import { ArticleNewReactiveComponent } from './articles/article-new-reactive/article-new-reactive.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'articles/list', component: ArticleListComponent },
+  { path: 'articles/create', component: ArticleNewReactiveComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
