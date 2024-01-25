f5d44186eb44899d191d9b24835d510ec84be9f8
utopia-code <utopiacode.developer@gmail.com>
2024-01-25 12:21:53 +0100

    PEC7 - Exercise 2.b: - add default and catch-all routes - correction in article/ paths - add article-detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { ArticleListComponent } from './articles/article-list/article-list.component';
 import { ArticleNewReactiveComponent } from './articles/article-new-reactive/article-new-reactive.component';
+import { ArticleDetailComponent } from './articles/article-detail/article-detail.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'articles/list', component: ArticleListComponent },
-  { path: 'articles/create', component: ArticleNewReactiveComponent },
+  { path: 'article/list', component: ArticleListComponent },
+  { path: 'article/create', component: ArticleNewReactiveComponent },
+  { path: 'article/:id', component: ArticleDetailComponent },
+  { path: '**', redirectTo: '/login'},
 ];
 
 @NgModule({
