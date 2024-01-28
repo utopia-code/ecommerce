1b9368489a1b051431bd93042a6f811cd92f72ea
utopia-code <utopiacode.developer@gmail.com>
2024-01-28 13:26:04 +0100

    PEC7 - Exercise 3: lazy loading: loading children in app routing and delete components of app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuardService } from './guards/auth-guard.service';
-
-import { LoginComponent } from './user/login/login.component';
-import { RegisterComponent } from './user/register/register.component';
-import { ArticleListComponent } from './articles/article-list/article-list.component';
-import { ArticleNewReactiveComponent } from './articles/article-new-reactive/article-new-reactive.component';
-import { ArticleDetailComponent } from './articles/article-detail/article-detail.component';
-import { ArticleNewDeactivateGuardService } from './guards/article-new-deactivate-guard.service';
-import { ArticleLoadResolverService } from './guards/article-load-resolver.service';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'article/list', component: ArticleListComponent, 
-    canActivate: [AuthGuardService]  },
-  { path: 'article/create', component: ArticleNewReactiveComponent, 
-    canActivate: [AuthGuardService],
-    canDeactivate: [ArticleNewDeactivateGuardService] },
-  { path: 'article/:id', component: ArticleDetailComponent, 
-    canActivate: [AuthGuardService],
-    resolve: { article: ArticleLoadResolverService} },
-  { path: '**', redirectTo: '/login'},
+  { path: '', redirectTo: 'user/login', pathMatch: 'full' },
+  { path: 'article', loadChildren: () => import('./articles/article.module').then(m => m.ArticleModule) },
+  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+  { path: '**', redirectTo: 'user/login'},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ArticleItemComponent } from './articles/article-item/article-item.component';
-import { ArticleListComponent } from './articles/article-list/article-list.component';
-import { ArticleDetailComponent } from './articles/article-detail/article-detail.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoginComponent } from './user/login/login.component';
-import { RegisterComponent } from './user/register/register.component';
-import { ArticleNewReactiveComponent } from './articles/article-new-reactive/article-new-reactive.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { DefectImgPipe } from './pipes/defect-img.pipe';
 
 import { ArticleService } from './services/article.service';
 import { UserService } from './services/user.service';
@@ -31,20 +23,11 @@ registerLocaleData(localeES, 'es-ES', localeESExtra);
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleItemComponent,
-    ArticleListComponent,
-    NavbarComponent,
-    ArticleNewReactiveComponent,
-    DefectImgPipe,
-    LoginComponent,
-    RegisterComponent,
-    ArticleDetailComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [
