0aabb5dd3694872ff8c5b2c32cf184e94596a52d
utopia-code <utopiacode.developer@gmail.com>
2024-01-31 15:00:42 +0100

    PEC7 - Delete tests and unneeded files

--- src/app/pipes/defect-img.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DefectImgPipe } from './defect-img.pipe';
-
-describe('DefectImgPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DefectImgPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
